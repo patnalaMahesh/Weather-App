a2686b5665b8593adf80f01caf93a707e6b66651
Mahesh Chandra Mouli <patnalamaheshchandramouli.com>
2025-07-07 16:30:03 +0530

    fixied issue in vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Weather-App/", // 👈 this must match your GitHub repo name exactly
+  base: "/", 
   plugins: [react()],
 })
